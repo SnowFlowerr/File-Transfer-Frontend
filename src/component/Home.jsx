b6e0f4ebe0c876a6e19cd26e23f75b55a8f791d1
SnowFlowerr <bhudeokrit@gmail.com>
2024-08-23 10:36:32 +0530

    first commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,6 +3,9 @@ import axios from 'axios'
 
 export default function Home() {
     const [file, setFile] = useState("")
+    const [img, setImg] = useState("")
+    const [err, setErr] = useState("")
+    
     function handleChange(e) {
         e.preventDefault();
         setFile(e.target.files[0])
@@ -12,10 +15,11 @@ export default function Home() {
         const formData=new FormData()
         formData.append('file',file)
         try{
-            const data=await axios.post('http://localhost:800/formData/data',formData)
-            // console.log(data.data)
+            const data=await axios.post('https://file-transfer-backend-production.up.railway.app/formData/data',formData)
+            setImg(data?.data)
         }
         catch(err){
+            setErr(err.message)
             console.log(err)
         }
         // .then(res=>console.log(res.data))
@@ -30,6 +34,10 @@ export default function Home() {
                 {file.type} <br />
                 {file.size} bytes <br />
                 <button onClick={sendFile}>Send</button>
+                <br />
+                {err&&err}
+                <a href={img?.image}>{img?.image}</a>
+                <img src={img?.image} alt="" width="100%"/>
             </form>
         </div>
     )
